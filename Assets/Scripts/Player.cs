283cf9cbe1c8502e230c65a2118bb122eb73a833
이하나 <45592710+leehana0921@users.noreply.github.com>
2025-06-02 03:19:05 +0900

    [Feat] 데미지 인터페이스 구현
    인터페이스로 데미지를 입을 수 있게끔 기능 구현

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour, IDamagable
+{
+    public bool IsControlActivate { get; set; } = true;
+
+    private PlayerStatus _status;
+
+    private void Init()
+    {
+        _status = GetComponent<PlayerStatus>();
+
+
+    }
+
+    public void TakeDamage(int value)
+    {
+        //_status.CurrentHp.Value -= value;
+
+        //if (_status.CurrentHp.Value <= 0) Dead();
+    }
+
+    public void Dead()
+    {
+        Debug.Log("플레이어 사망 처리");
+    }
+}
